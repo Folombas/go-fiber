bb2cbf36145dd53338f283b718baba8b7a8c8c2f
Folombas <folombas@yandex.ru>
2025-06-01 16:56:56 +0300

    Start again!

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"folocode/go-fiber/config"
-	"folocode/go-fiber/internal/home"
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/recover"
-)
+import "fmt"
 
 func main() {
-	config.Init()
-	dbConf := config.NewDatabaseConfig()
-	log.Println(dbConf)
-	app := fiber.New()
-	app.Use(recover.New())
-
-	home.NewHandler(app)
-
-	app.Listen(":3000")
+	fmt.Println("Start")
 }
