9b2dc75b3db590008de74b430a45dcc2cce226fb
Folombas <folombas@yandex.ru>
2025-06-05 22:42:03 +0300

    Custom logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"folocode/go-fiber/config"
 	"folocode/go-fiber/internal/home"
+	"folocode/go-fiber/pkg/logger"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/rs/zerolog"
 )
 
 func main() {
 	config.Init()
 	config.NewDatabaseConfig()
 	logConfig := config.NewLogConfig()
+	customLogger := logger.NewLogger(logConfig)
 
 	app := fiber.New()
-	zerolog.SetGlobalLevel(zerolog.Level(logConfig.Level))
-	app.Use(fiberzerolog.New())
+	app.Use(fiberzerolog.New(fiberzerolog.Config{
+		Logger: customLogger,
+	}))
 	app.Use(recover.New())
 
-	home.NewHandler(app)
+	home.NewHandler(app, customLogger)
 
 	app.Listen(":8080")
 }
