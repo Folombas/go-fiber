12a945536a0bad26931fa7bbea2cbef6fe3ed6c4
Folombas <folombas@yandex.ru>
2025-06-15 23:10:01 +0300

    Migrations

--- internal/vacancy/repository.go
+++ internal/vacancy/repository.go
@@ -3,6 +3,7 @@ package vacancy
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -10,26 +11,27 @@ import (
 )
 
 type VacancyRepository struct {
-	Dbpool *pgxpool.Pool
+	Dbpool       *pgxpool.Pool
 	CustomLogger *zerolog.Logger
 }
 
 func NewVacancyRepository(dbpool *pgxpool.Pool, customLogger *zerolog.Logger) *VacancyRepository {
-	return &VacancyRepository {
-		Dbpool: dbpool,
+	return &VacancyRepository{
+		Dbpool:       dbpool,
 		CustomLogger: customLogger,
 	}
 }
 
 func (r *VacancyRepository) addVacancy(form VacancyCreateForm) error {
-	query := `INSERT INTO vacancies (email, role, company, salary, type, location) VALUES (@email, @role, @company, @salary, @type, @location)`
-	args := pgx.NamedArgs {
-		"email": form.Email,
-		"role": form.Role,
-		"company": form.Company,
-		"salary": form.Salary,
-		"type": form.Type,
-		"location": form.Location,
+	query := `INSERT INTO vacancies (email, role, company, salary, type, location, createdat) VALUES (@email, @role, @company, @salary, @type, @location, @createdat)`
+	args := pgx.NamedArgs{
+		"email":     form.Email,
+		"role":      form.Role,
+		"company":   form.Company,
+		"salary":    form.Salary,
+		"type":      form.Type,
+		"location":  form.Location,
+		"createdat": time.Now(),
 	}
 	_, err := r.Dbpool.Exec(context.Background(), query, args)
 	if err != nil {
